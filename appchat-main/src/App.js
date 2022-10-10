a05cc7ad4bd86f554069cb3821bcc27d127ba637
Dang Khoa <vodangkhoa835@gmail.com>
2022-10-11 02:08:59 +0700

    direct login form

--- appchat-main/src/App.js
+++ appchat-main/src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import './App.css';
 import DefaultLayout from './component/Chat'
 import Login from './component/Login/Login';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
+import {useState} from 'react'
+
 function App() {
+  const [ checkLogin, setCheckLogin ] = useState(false)
   return (
     <BrowserRouter>
       <div>
         <Routes>
-        <Route path='/login' element= {<Login />} />
-        <Route path='/' element= {<DefaultLayout />} />
+          <Route path='/login' element= {<Login />} />
+          <Route element= {<PrivateRoute isLogged={checkLogin} />} >
+            <Route path='/' element= {<DefaultLayout />} />
+            <Route path='*' />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
