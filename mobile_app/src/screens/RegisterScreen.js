088a0691b174f9604238b3889575e62c664d589a
Nguyễn Thái Nguyên <thnguyen.7620@gmail.com>
2022-10-30 15:45:43 +0700

    login

--- mobile_app/src/screens/RegisterScreen.js
+++ mobile_app/src/screens/RegisterScreen.js
@@ -1,11 +1,12 @@
 import { Button, Image, KeyboardAvoidingView, PermissionsAndroid, Platform, 
     StyleSheet, Text, TextInput, TouchableOpacity, View, Alert, Linking, Modal, 
-    TouchableWithoutFeedback} from 'react-native'
+    TouchableWithoutFeedback,
+    Keyboard,
+    ScrollView} from 'react-native'
 import React, { useState } from 'react'
 import RadioGroup from 'react-native-radio-buttons-group';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import Ionicons from 'react-native-vector-icons/Ionicons'
-// import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import * as ImagePicker from 'expo-image-picker';
 
 const radioButtonsData = [{
@@ -18,9 +19,20 @@ const radioButtonsData = [{
     value: 'Nữ'
 }]
 export default function RegisterScreen() {
+    const [show,setShow] = useState(false)
+    const [visible,setVisible] = useState(true)
     const [avatarSource,setAvatarSource] = useState('');
     const [modalVisible, setModalVisible] = useState(false);
     const [radioButtons, setRadioButtons] = useState(radioButtonsData)
+    const [alert, setAlert] = useState('dd')
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [cfpassword,SetCfpassword] = useState('');
+    const [username,setUsername] = useState('');
+    const [birthday,setBirthday] = useState('');
+    const [gender,setGender] = useState('');
+
+
     function onPressRadioButton(radioButtonsArray) {
         setRadioButtons(radioButtonsArray);
     }
@@ -30,7 +42,7 @@ export default function RegisterScreen() {
   
     const handleConfirm = (date) => {
         let tempDate = new Date(date);
-        let fDate =tempDate.getDate()+"/"+tempDate.getMonth()+"/"+tempDate.getFullYear();
+        let fDate =tempDate.getDate()+"/"+(tempDate.getMonth()*1+1)+"/"+tempDate.getFullYear();
         setTextDate(fDate);
         setDatePickerVisibility(false);
     };
@@ -42,7 +54,7 @@ export default function RegisterScreen() {
           Linking.openSettings();
         }
     };
-    const [image, setImage] = useState('https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=170667a&w=0&h=kEAA35Eaz8k8A3qAGkuY8OZxpfvn9653gDjQwDHZGPE=');
+    const [image, setImage] = useState('https://firebasestorage.googleapis.com/v0/b/chatapp-group9.appspot.com/o/avatar.png?alt=media&token=f69725be-13aa-4746-9d6c-39fa0f7d2910');
 
     const showImagePicker = async () => {
         // Ask the user for the permission to access the media library 
@@ -67,7 +79,7 @@ export default function RegisterScreen() {
         }
         setModalVisible(false);
         const result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
@@ -104,7 +116,7 @@ export default function RegisterScreen() {
         setModalVisible(false);
         const result = await ImagePicker.launchCameraAsync(
             {
-                mediaTypes: ImagePicker.MediaTypeOptions.All,
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
                 allowsEditing: true,
                 aspect: [4, 3],
                 quality: 1,
@@ -119,107 +131,155 @@ export default function RegisterScreen() {
           
         }
     }
+    const register = async () => {
+        if(password === cfpassword){
+            try {
+                const registerData = await registerUser(registerForm)
+                if (!registerData.success) {
+                    setAlert({ type: 'danger', message: registerData.message })
+                    setTimeout(() => setAlert(null), 5000)
+                }else{
+                    setAlert({ type: 'danger', message: "Thành công đăng ký tài khoản" })
+                    setTimeout(() => setAlert(null), 10000)
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        else{
+            setAlert({ type: 'danger', message: "Mật khẩu không khớp" })
+            setTimeout(() => setAlertRe(null), 5000)
+        }
+
+        
+    }
 
   return (
     <View style={styles.container}>
-        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
-            <View style={{width:'100%',alignItems:'center'}}>
-                <Image 
-                    style={styles.avatar}
-                    // source={require('../image/avatar.png')}
-                    source ={{uri:image}}
-                />
-                <TouchableOpacity 
-                    style={styles.btnChooseImage}
-                    onPress={()=>setModalVisible(true)}>
-                    <Text style={styles.textChooseImage}>Chọn ảnh đại diện</Text>
-                </TouchableOpacity>
-                <Modal
-                    visible={modalVisible}
-                    transparent={true}
-                    onRequestClose={() => setModalVisible(false)}
-                    animationType='slide'
-                    hardwareAccelerated
-                >
-                    <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
-                        <View style={styles.centered_view} >
-                            <View style={styles.modal_cont}>
-                                <Text style={styles.modal_title}>Tải lên hình ảnh</Text>
-                                <View style={styles.modal_body}>
-                                    <TouchableOpacity
-                                        onPress={() => openCamera()}
-                                        style={styles.choose}>
-                                        <Ionicons name='camera-outline' size={26} color={'#056282'}/>
-                                        <Text style={styles.text_choose}>Chụp ảnh mới</Text>
-                                    </TouchableOpacity>
-                                    <TouchableOpacity
-                                        onPress={() => showImagePicker()}
-                                        style={styles.choose}>
-                                        <Ionicons name='images-outline' size={26} color={'#056282'}/>
-                                        <Text style={styles.text_choose}>Chọn ảnh từ thiết bị</Text>
-                                    </TouchableOpacity>
-                                    <TouchableOpacity
-                                        onPress={() => setModalVisible(false)}
-                                        style={styles.choose}>
-                                        <Ionicons name='close-circle-outline' size={26} color={'#056282'}/>
-                                        <Text style={styles.text_choose}>Hủy</Text>
-                                    </TouchableOpacity>
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>   
+        <ScrollView>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={{width:'100%',alignItems:'center',}}>
+                    <Image 
+                        style={styles.avatar}
+                        // source={require('../image/avatar.png')}
+                        source ={{uri:image}}
+                    />
+                    <TouchableOpacity 
+                        style={styles.btnChooseImage}
+                        onPress={()=>setModalVisible(true)}>
+                        <Text style={styles.textChooseImage}>Chọn ảnh đại diện</Text>
+                    </TouchableOpacity>
+                    <Modal
+                        visible={modalVisible}
+                        transparent={true}
+                        onRequestClose={() => setModalVisible(false)}
+                        animationType='slide'
+                        hardwareAccelerated>
+                        <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
+                            <View style={styles.centered_view} >
+                                <View style={styles.modal_cont}>
+                                    <Text style={styles.modal_title}>Tải lên hình ảnh</Text>
+                                    <View style={styles.modal_body}>
+                                        <TouchableOpacity
+                                            onPress={() => openCamera()}
+                                            style={styles.choose}>
+                                            <Ionicons name='camera-outline' size={26} color={'#056282'}/>
+                                            <Text style={styles.text_choose}>Chụp ảnh mới</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity
+                                            onPress={() => showImagePicker()}
+                                            style={styles.choose}>
+                                            <Ionicons name='images-outline' size={26} color={'#056282'}/>
+                                            <Text style={styles.text_choose}>Chọn ảnh từ thiết bị</Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity
+                                            onPress={() => setModalVisible(false)}
+                                            style={styles.choose}>
+                                            <Ionicons name='close-circle-outline' size={26} color={'#056282'}/>
+                                            <Text style={styles.text_choose}>Hủy</Text>
+                                        </TouchableOpacity>
+                                    </View>
                                 </View>
                             </View>
-                        </View>
-                    </TouchableWithoutFeedback>
-                </Modal>
-                <View style={styles.input_cont}>
-                    <Ionicons name='mail-outline' size={25} style={styles.icon}/>
-                    <TextInput
-                        style={styles.input}
-                        placeholder='Email'/>
-                </View>
-                <View style={styles.input_cont}>
-                    <Ionicons name='lock-closed-outline' size={25} style={styles.icon}/>
-                    <TextInput
-                        style={styles.input}
-                        placeholder='Mật khẩu'/>
-                </View>
-                <View style={styles.input_cont}>
-                    <Ionicons name='lock-closed-outline' size={25} style={styles.icon}/>
-                    <TextInput
-                        style={styles.input}
-                        placeholder='Nhập lại mật khẩu'/>
-                </View>
-                <View style={styles.input_cont}>
-                    <Ionicons name='person-outline' size={25} style={styles.icon}/>
-                    <TextInput
-                        style={styles.input}
-                        placeholder='User name'/>
-                </View>
-                
-                <TouchableOpacity
-                    style={styles.input_cont}
-                    onPress={()=>setDatePickerVisibility(true)}>
-                    <Ionicons name='calendar-outline' size={25} style={styles.icon}/>
-                    <Text style={{fontSize:16}}>{textdate}</Text>
-                </TouchableOpacity>
-                <DateTimePickerModal
-                    isVisible={isDatePickerVisible}
-                    mode="date"
-                    onConfirm={handleConfirm}
-                    onCancel={()=>setDatePickerVisibility(false)}
-                />
-                <View style={styles.GT}>
-                    <Text style={{fontSize:16,marginRight:50}}>Giới tính:</Text>
-                    <RadioGroup
-                        layout='row'
-                        radioButtons={radioButtons}
-                        onPress={onPressRadioButton}
+                        </TouchableWithoutFeedback>
+                    </Modal>
+                    <View style={styles.input_cont}>
+                        <Ionicons name='mail-outline' size={25} color={'#056282'} style={styles.icon}/>
+                        <TextInput
+                            style={styles.input}
+                            placeholder='Email'/>
+                    </View>
+                    <View style={styles.input_cont}>
+                        <Ionicons name='lock-closed-outline' size={25} color={'#056282'} style={styles.icon}/>
+                        <TextInput
+                            style={styles.input_pass}
+                            placeholder='Mật khẩu'
+                            secureTextEntry={visible}/>
+                        <TouchableOpacity
+                            style={{width:'10%'}}
+                            onPress={()=>{
+                            setShow(!show);
+                            setVisible(!visible);
+                            }}>
+                            <Ionicons
+                                name={show === false ? 'eye-outline':'eye-off-outline'}
+                                size={26}
+                                />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.input_cont}>
+                        <Ionicons name='lock-closed-outline' size={25} color={'#056282'} style={styles.icon}/>
+                        <TextInput
+                            style={styles.input_pass}
+                            placeholder='Nhập lại mật khẩu'
+                            secureTextEntry={visible}/>
+                        <TouchableOpacity
+                            style={{width:'10%'}}
+                            onPress={()=>{
+                            setShow(!show);
+                            setVisible(!visible);
+                            }}>
+                            <Ionicons
+                                name={show === false ? 'eye-outline':'eye-off-outline'}
+                                size={26}
+                                />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.input_cont}>
+                        <Ionicons name='person-outline' size={25}  color={'#056282'} style={styles.icon}/>
+                        <TextInput
+                            style={styles.input}
+                            placeholder='User name'/>
+                    </View>
+                    <TouchableOpacity
+                        style={styles.input_cont}
+                        onPress={()=>setDatePickerVisibility(true)}>
+                        <Ionicons name='calendar-outline' size={25} color={'#056282'} style={styles.icon}/>
+                        <Text style={{fontSize:16}}>{textdate}</Text>
+                    </TouchableOpacity>
+                    <DateTimePickerModal
+                        isVisible={isDatePickerVisible}
+                        mode="date"
+                        onConfirm={handleConfirm}
+                        onCancel={()=>setDatePickerVisibility(false)}
                     />
+                    <View style={styles.GT}>
+                        <Text style={{fontSize:16,marginRight:50}}>Giới tính:</Text>
+                        <RadioGroup
+                            layout='row'
+                            radioButtons={radioButtons}
+                            onPress={onPressRadioButton}
+                        />
+                    </View>
+                    <Text style={{color:'#F15151',fontSize:16, marginTop:20,}}>{alert}</Text>
+                    <TouchableOpacity 
+                        style={styles.btnRegister}>
+                        <Text style={{fontSize:16,color:'#fff'}}>Đăng ký</Text>
+                    </TouchableOpacity>
                 </View>
-                <TouchableOpacity 
-                    style={styles.btnRegister}>
-                    <Text style={{fontSize:16,color:'#fff'}}>Đăng ký</Text>
-                </TouchableOpacity>
-
-            </View>
+            </TouchableWithoutFeedback>
+        </ScrollView>
         </KeyboardAvoidingView>
     </View>
   )
@@ -231,7 +291,7 @@ const styles = StyleSheet.create({
         justifyContent:'center',
     },
     avatar:{
-        marginTop:-35,
+        marginTop:40,
         width:150,
         height:150,
         borderRadius:100,
@@ -242,7 +302,7 @@ const styles = StyleSheet.create({
         borderRadius:100,
         padding:10,
         marginTop:10,
-        marginBottom:30,
+        marginBottom:20,
     },
     textChooseImage:{
         fontSize:16,
@@ -259,6 +319,10 @@ const styles = StyleSheet.create({
         fontSize:16,
         width:'90%',
     },
+    input_pass:{
+        fontSize:16,
+        width:'80%',
+    },
     GT:{
         flexDirection:'row',
         justifyContent:'flex-start',
@@ -273,7 +337,8 @@ const styles = StyleSheet.create({
         justifyContent:'center',
         height:50,
         borderRadius:20,
-        marginTop:20,
+        marginTop:10,
+        marginBottom:40,
     },
     icon:{
         marginRight:10,
